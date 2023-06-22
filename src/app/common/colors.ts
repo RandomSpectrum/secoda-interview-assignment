eeef70b1a9a73b425772c191dcd969e8bd442683
Alex <alex@Alexs-MacBook-Air.local>
2023-06-22 14:28:03 -0300

    Added border styling to crypto table

---
+++ src/app/common/colors.ts
@@ -0,0 +1,5 @@
+const colors = {
+  tableBorder: '#c7c7c7'
+}
+
+export default colors
--- src/app/crypto-listing/crypto-table.tsx
+++ src/app/crypto-listing/crypto-table.tsx
@@ -8,25 +8,30 @@ import CryptoTableRow from './crypto-table-row';
 import Paper from '@mui/material/Paper';
 import { useAppSelector } from '../store'
 import { CryptocurrencyType } from './types';
+import colors from '../common/colors';
 
 const CryptoTable = () => {
   const cryptocurrencies = useAppSelector((state) => state.cryptoListing.cryptocurrencies)
 
-  return (<TableContainer component={Paper}>
-    <Table aria-label="simple table">
-      <TableHead>
-        <TableRow>
-          <TableCell><strong>Name</strong></TableCell>
-          <TableCell align="right"><strong>Price</strong></TableCell>
-          <TableCell align="right"><strong>Marketcap</strong></TableCell>
-          <TableCell align="right"><strong>Volume (24H)</strong></TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {cryptocurrencies.map((cryptocurrency: CryptocurrencyType) => (<CryptoTableRow key={cryptocurrency.name} cryptocurrency={cryptocurrency} />))}
-      </TableBody>
-    </Table>
-  </TableContainer>)
+  return (
+    <TableContainer
+      component={Paper}
+      style={{ border: `1px solid ${colors.tableBorder}`, borderRadius: 3 }}
+    >
+      <Table aria-label="simple table" >
+        <TableHead>
+          <TableRow>
+            <TableCell><strong>Name</strong></TableCell>
+            <TableCell align="right"><strong>Price</strong></TableCell>
+            <TableCell align="right"><strong>Marketcap</strong></TableCell>
+            <TableCell align="right"><strong>Volume (24H)</strong></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {cryptocurrencies.map((cryptocurrency: CryptocurrencyType) => (<CryptoTableRow key={cryptocurrency.name} cryptocurrency={cryptocurrency} />))}
+        </TableBody>
+      </Table>
+    </TableContainer>)
 }
 
 export default CryptoTable
